5b1012a529fea9e35eb3a1055c489c85c54d55a6
Brian Magaha <btmy87@gmail.com>
2024-09-14 19:32:53 -0400

    Refactor make_badge.py as a module to make linter happy

--- make_badge.py
+++ make_badge.py
@@ -1,5 +1,4 @@
-### make_badge.py
-### generate a combined svg file from all workflows
+"""generate a combined svg file from all workflows"""
 
 import glob
 import json
@@ -12,67 +11,73 @@ OS = {"ubuntu-latest": "Linux",
       "macos-latest": "Mac", 
       "macos-14": "Mac14",
       "macos-13": "Mac13" }
-STATUS = {"pass": "blue", 
+STATUS = {"pass": "blue",
           "fail": "red"}
 OFFSETX = 2
 OFFSETY = 2
 MAX_WIDTH = 400
 
-badge = ""
-x = 0
-y = 0
-width = 0
-height = 0
-for i, file in enumerate(glob.glob("./artifacts/*.json")):
-    print(f"reading file {i}: {file}")
-    with open(file, "r", encoding="utf-8") as fid:
-        res = json.load(fid)
+def make_badge():
+    """make combined badge summarizing test runs."""
 
-    # Get badge from shields.io
-    url = BASE_URL + f'{OS[res["os"]]}:{res["version"]}-{res["status"]}-{STATUS[res["status"]]}'
-    print(f"Getting badge from url: {url}")
+    badge = ""
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+    for i, file in enumerate(glob.glob("./artifacts/*.json")):
+        print(f"reading file {i}: {file}")
+        with open(file, "r", encoding="utf-8") as fid:
+            res = json.load(fid)
 
-    req = urllib.request.Request(url)
-    req.add_header("Content-Type", "text/plain")
-    req.add_header("User-Agent", "MyBadge/1.0")
-    with urllib.request.urlopen(req) as fid:
-        tempBadge = fid.read().decode("utf-8")
+        # Get badge from shields.io
+        url = BASE_URL + f'{OS[res["os"]]}:{res["version"]}-{res["status"]}-{STATUS[res["status"]]}'
+        print(f"Getting badge from url: {url}")
 
-    # Each badge needs a unique url for clipping path
-    tempBadge2 = tempBadge.replace('id="r"', f'id="r{i}"')
-    tempBadge3 = tempBadge2.replace('url(#r)', f'url(#r{i})')
+        req = urllib.request.Request(url)
+        req.add_header("Content-Type", "text/plain")
+        req.add_header("User-Agent", "MyBadge/1.0")
+        with urllib.request.urlopen(req) as fid:
+            temp_badge = fid.read().decode("utf-8")
 
-    # need x and y offset for each badge
-    idx = tempBadge3.index("aria-label")
-    tempBadge4 = (tempBadge3[:idx]
-                  + f' x="{x}" y="{y}" '
-                  + tempBadge3[idx:])
+        # Each badge needs a unique url for clipping path
+        temp_badge2 = temp_badge.replace('id="r"', f'id="r{i}"')
+        temp_badge3 = temp_badge2.replace('url(#r)', f'url(#r{i})')
 
-    # Get offset and width data
-    m = re.search(r'width="(\d+)"', tempBadge4)
-    widthStr = m.groups(0)[0]
-    m = re.search(r'height="(\d+)"', tempBadge4)
-    heightStr = m.groups(0)[0]
+        # need x and y offset for each badge
+        idx = temp_badge3.index("aria-label")
+        temp_badge4 = (temp_badge3[:idx]
+                      + f' x="{x}" y="{y}" '
+                      + temp_badge3[idx:])
 
-    # update height, width, and offsets
-    x = x + int(widthStr) + OFFSETX
-    width = max(width, x)
-    if x > MAX_WIDTH:
-        x = 0
-        y = y + int(heightStr) + OFFSETY
+        # Get offset and width data
+        m = re.search(r'width="(\d+)"', temp_badge4)
+        width_str = m.groups(0)[0]
+        m = re.search(r'height="(\d+)"', temp_badge4)
+        height_str = m.groups(0)[0]
 
-    height = y + int(heightStr)
+        # update height, width, and offsets
+        x = x + int(width_str) + OFFSETX
+        width = max(width, x)
+        if x > MAX_WIDTH:
+            x = 0
+            y = y + int(height_str) + OFFSETY
 
-    # accumulate badge
-    badge += tempBadge4
+        height = y + int(height_str)
 
-# make combined badge
-badge = ('<svg xmlns="http://www.w3.org/2000/svg"'
-         + ' xmlns:xlink="http://www.w3.org/1999/xlink" role="img"'
-         + f' height="{height}" width="{width}">'
-         + badge
-         + '</svg>')
+        # accumulate badge
+        badge += temp_badge4
 
-with open("./artifacts/badge.svg", "w+", encoding="utf-8") as fid:
-    fid.write(badge)
+    # make combined badge
+    badge = ('<svg xmlns="http://www.w3.org/2000/svg"'
+            + ' xmlns:xlink="http://www.w3.org/1999/xlink" role="img"'
+            + f' height="{height}" width="{width}">'
+            + badge
+            + '</svg>')
 
+    with open("./artifacts/badge.svg", "w+", encoding="utf-8") as fid:
+        fid.write(badge)
+
+
+if __name__ == "__main__":
+    make_badge()
